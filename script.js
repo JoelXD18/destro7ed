222d4b53babe2663c8c47fa3ccccd6b11463906d
JoelXD18 <96124649+JoelXD18@users.noreply.github.com>
2024-11-06 09:29:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,19 @@ function showSection(section) {
     });
     document.getElementById(section).classList.add('active');
   }
-  
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDBJZdUYznvwU_YDDjjEVjA6oW8Bgxg6Qo",
+  authDomain: "message-801a6.firebaseapp.com",
+  databaseURL: "https://message-801a6-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "message-801a6",
+  storageBucket: "message-801a6.firebasestorage.app",
+  messagingSenderId: "930429385952",
+  appId: "1:930429385952:web:331b467af4cd6902806485",
+  measurementId: "G-GBJWMHPQJ6"
+};
+
+// Inicializar Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const database = firebase.database(app);
